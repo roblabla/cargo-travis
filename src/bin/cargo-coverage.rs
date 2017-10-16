a27f0f0588a9f07d15287bd64a091c5663d74f19
roblabla <unfiltered@roblab.la>
2017-10-16 15:01:16 +0200

    Update to cargo 0.22

--- src/bin/cargo-coverage.rs
+++ src/bin/cargo-coverage.rs
@@ -3,13 +3,14 @@ extern crate cargo_travis;
 extern crate env_logger;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate log;
 
 use std::env;
 use std::path::Path;
 use cargo_travis::{CoverageOptions, build_kcov};
 use cargo::core::{Workspace};
 use cargo::util::{CargoError, CargoErrorKind, Config, CliResult, CliError};
-use cargo::core::shell::{Verbosity}; 
 use cargo::ops::{Packages, MessageFormat};
 
 pub const USAGE: &'static str = "
@@ -22,6 +23,8 @@ Usage:
 Coverage Options:
     -m PATH, --merge-into PATH   Path to the directory to put the final merged
                                  kcov result into [default: target/kcov]
+    --exclude-pattern PATTERN    Comma-separated path patterns to exclude from the report
+
 Test Options:
     -h, --help                   Print this message
     --lib                        Test only this package's library
@@ -29,8 +32,12 @@ Test Options:
     --bins                       Test all binaries
     --test NAME                  Test only the specified integration test target
     --tests                      Test all tests
+    --bench NAME ...             Test only the specified bench target
+    --benches                    Test all benches
+    --all-targets                Test all targets (default)
     -p SPEC, --package SPEC ...  Package to run tests for
     --all                        Test all packages in the workspace
+    --exclude SPEC ...           Exclude packages from the test
     -j N, --jobs N               Number of parallel jobs, defaults to # of CPUs
     --release                    Build artifacts in release mode, with optimizations
     --features FEATURES          Space-separated list of features to also build
@@ -38,28 +45,24 @@ Test Options:
     --no-default-features        Do not build the `default` feature
     --target TRIPLE              Build for the target triple
     --manifest-path PATH         Path to the manifest to build tests for
-    --exclude-pattern PATTERN    Comma-separated  path patterns to exclude from the report
     -v, --verbose ...            Use verbose output
     -q, --quiet                  No output printed to stdout
     --color WHEN                 Coloring: auto, always, never
     --no-fail-fast               Run all tests regardless of failure
     --frozen                     Require Cargo.lock and cache are up to date
     --locked                     Require Cargo.lock is up to date
+    -Z FLAG ...                  Unstable (nightly-only) flags to Cargo
 ";
 
 
 #[derive(Deserialize)]
 pub struct Options {
     arg_args: Vec<String>,
-    flag_all_features: bool,
-    flag_merge_into: String,
     flag_features: Vec<String>,
+    flag_all_features: bool,
     flag_jobs: Option<u32>,
     flag_manifest_path: Option<String>,
-    flag_exclude_pattern: Option<String>,
     flag_no_default_features: bool,
-    flag_no_fail_fast: bool,
-    flag_all: bool,
     flag_package: Vec<String>,
     flag_target: Option<String>,
     flag_lib: bool,
@@ -67,30 +70,42 @@ pub struct Options {
     flag_bins: bool,
     flag_test: Vec<String>,
     flag_tests: bool,
+    flag_bench: Vec<String>,
+    flag_benches: bool,
+    flag_all_targets: bool,
     flag_verbose: u32,
     flag_quiet: Option<bool>,
     flag_color: Option<String>,
     flag_release: bool,
+    flag_no_fail_fast: bool,
     flag_frozen: bool,
     flag_locked: bool,
+    flag_all: bool,
+    flag_exclude: Vec<String>,
+    #[serde(rename = "flag_Z")]
+    flag_z: Vec<String>,
+
+    // cargo-coverage flags
+    flag_exclude_pattern: Option<String>,
+    flag_merge_into: String
 }
 
 fn execute(options: Options, config: &Config) -> CliResult {
+    debug!("executing; cmd=cargo-coverage; args={:?}",
+           env::args().collect::<Vec<_>>());
+
     let kcov_path = build_kcov();
+    // TODO: build_kcov() - Might be a good idea to consider linking kcov as a
     // lib instead ?
     try!(config.configure(options.flag_verbose,
                           options.flag_quiet,
                           &options.flag_color,
                           options.flag_frozen,
-                          options.flag_locked));
+                          options.flag_locked,
+                          &options.flag_z));
 
     let root = try!(cargo::util::important_paths::find_root_manifest_for_wd(options.flag_manifest_path, config.cwd()));
-
-    let spec = if options.flag_all {
-        Packages::All
-    } else {
-        Packages::Packages(&options.flag_package)
-    };
+    let ws = try!(Workspace::new(&root, config));
 
     let empty = vec![];
     let (mode, filter) = (cargo::ops::CompileMode::Test, cargo::ops::CompileFilter::new(
@@ -101,12 +116,17 @@ fn execute(options: Options, config: &Config) -> CliResult {
         options.flag_tests,
         &empty,
         false,
-        &empty,
-        false,
+        &options.flag_bench,
+        options.flag_benches,
+        options.flag_all_targets
     ));
 
+    let spec = try!(Packages::from_flags(ws.is_virtual(), options.flag_all, &options.flag_exclude, &options.flag_package));
+
     // TODO: Shouldn't this be in run_coverage ?
+    // TODO: It'd be nice if there was a flag in compile_opts for this.
     std::env::set_var("RUSTFLAGS", "-C link-dead-code");
+
     let ops = CoverageOptions {
         merge_dir: Path::new(&options.flag_merge_into),
         merge_args: vec![],
@@ -130,8 +150,6 @@ fn execute(options: Options, config: &Config) -> CliResult {
         },
     };
 
-    let ws = try!(Workspace::new(&root, config));
-
     let err = try!(cargo_travis::run_coverage(&ws, &ops, &options.arg_args));
 
     match err {
@@ -151,21 +169,18 @@ fn main() {
         Ok(cfg) => cfg,
         Err(e) => {
              let mut shell = cargo::core::Shell::new();
-             shell.set_verbosity(Verbosity::Verbose);
-             shell.set_color_choice(Some("auto")).unwrap();
              cargo::exit_with_error(e.into(), &mut shell)
         }
     };
     let result = (|| {
-        let args_result: Result<Vec<String>, CargoError> = env::args_os()
+        let args: Vec<_> = try!(env::args_os()
             .map(|s| {
                 s.into_string().map_err(|s| {
-                    format!("invalid unicode in argument: {:?}", s).into()
+                    CargoError::from(format!("invalid unicode in argument: {:?}", s))
                 })
             })
-            .collect();
-        let rest = &args_result?;
-        config.shell().set_verbosity(Verbosity::Verbose);
+            .collect());
+        let rest = &args;
         cargo::call_main_without_stdin(execute, &config, USAGE, rest, false)
     })();
     match result {
--- src/bin/cargo-coveralls.rs
+++ src/bin/cargo-coveralls.rs
@@ -3,14 +3,15 @@ extern crate cargo_travis;
 extern crate env_logger;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate log;
 
 use std::env;
 use std::path::Path;
 use cargo_travis::{CoverageOptions, build_kcov};
 use cargo::core::{Workspace};
-use cargo::util::{Config, CliResult, CargoErrorKind, CliError};
+use cargo::util::{CargoError, CargoErrorKind, Config, CliResult, CliError};
 use cargo::ops::{Packages, MessageFormat};
-use cargo::{call_main_without_stdin, exit_with_error};
 
 pub const USAGE: &'static str = "
 Record coverage of `cargo test`, this runs all binaries that `cargo test` runs
@@ -19,6 +20,9 @@ but not doc tests. The results of all tests are sent to coveralls.io
 Usage:
     cargo coveralls [options] [--] [<args>...]
 
+Coveralls Options:
+    --exclude-pattern PATTERN    Comma-separated  path patterns to exclude from the report
+
 Test Options:
     -h, --help                   Print this message
     --lib                        Test only this package's library
@@ -26,8 +30,12 @@ Test Options:
     --bins                       Test all binaries
     --test NAME                  Test only the specified integration test target
     --tests                      Test all tests
+    --bench NAME ...             Test only the specified bench target
+    --benches                    Test all benches
+    --all-targets                Test all targets (default)
     -p SPEC, --package SPEC ...  Package to run tests for
     --all                        Test all packages in the workspace
+    --exclude SPEC ...           Exclude packages from the test
     -j N, --jobs N               Number of parallel jobs, defaults to # of CPUs
     --release                    Build artifacts in release mode, with optimizations
     --features FEATURES          Space-separated list of features to also build
@@ -35,26 +43,24 @@ Test Options:
     --no-default-features        Do not build the `default` feature
     --target TRIPLE              Build for the target triple
     --manifest-path PATH         Path to the manifest to build tests for
-    --exclude-pattern PATTERN    Comma-separated  path patterns to exclude from the report
     -v, --verbose ...            Use verbose output
     -q, --quiet                  No output printed to stdout
     --color WHEN                 Coloring: auto, always, never
     --no-fail-fast               Run all tests regardless of failure
     --frozen                     Require Cargo.lock and cache are up to date
     --locked                     Require Cargo.lock is up to date
+    -Z FLAG ...                  Unstable (nightly-only) flags to Cargo
 ";
 
 
 #[derive(Deserialize)]
 pub struct Options {
     arg_args: Vec<String>,
-    flag_all_features: bool,
     flag_features: Vec<String>,
+    flag_all_features: bool,
     flag_jobs: Option<u32>,
     flag_manifest_path: Option<String>,
-    flag_exclude_pattern: Option<String>,
     flag_no_default_features: bool,
-    flag_all: bool,
     flag_package: Vec<String>,
     flag_target: Option<String>,
     flag_lib: bool,
@@ -62,16 +68,29 @@ pub struct Options {
     flag_bins: bool,
     flag_test: Vec<String>,
     flag_tests: bool,
+    flag_bench: Vec<String>,
+    flag_benches: bool,
+    flag_all_targets: bool,
     flag_verbose: u32,
     flag_quiet: Option<bool>,
     flag_color: Option<String>,
-    flag_no_fail_fast: bool,
     flag_release: bool,
+    flag_no_fail_fast: bool,
     flag_frozen: bool,
     flag_locked: bool,
+    flag_all: bool,
+    flag_exclude: Vec<String>,
+    #[serde(rename = "flag_Z")]
+    flag_z: Vec<String>,
+
+    // cargo-coveralls flags
+    flag_exclude_pattern: Option<String>,
 }
 
 fn execute(options: Options, config: &Config) -> CliResult {
+    debug!("executing; cmd=cargo-coveralls; args={:?}",
+           env::args().collect::<Vec<_>>());
+
     let kcov_path = build_kcov();
     // TODO: build_kcov() - Might be a good idea to consider linking kcov as a
     // lib instead ?
@@ -79,15 +98,11 @@ fn execute(options: Options, config: &Config) -> CliResult {
                           options.flag_quiet,
                           &options.flag_color,
                           options.flag_frozen,
-                          options.flag_locked));
+                          options.flag_locked,
+                          &options.flag_z));
 
     let root = try!(cargo::util::important_paths::find_root_manifest_for_wd(options.flag_manifest_path, config.cwd()));
-
-    let spec = if options.flag_all {
-        Packages::All
-    } else {
-        Packages::Packages(&options.flag_package)
-    };
+    let ws = try!(Workspace::new(&root, config));
 
     let empty = vec![];
     let (mode, filter) = (cargo::ops::CompileMode::Test, cargo::ops::CompileFilter::new(
@@ -98,9 +113,12 @@ fn execute(options: Options, config: &Config) -> CliResult {
         options.flag_tests,
         &empty,
         false,
-        &empty,
-        false,
+        &options.flag_bench,
+        options.flag_benches,
+        options.flag_all_targets
     ));
+
+    let spec = try!(Packages::from_flags(ws.is_virtual(), options.flag_all, &options.flag_exclude, &options.flag_package));
 
     // TODO: Shouldn't this be in run_coverage ?
     // TODO: It'd be nice if there was a flag in compile_opts for this.
@@ -132,8 +150,6 @@ fn execute(options: Options, config: &Config) -> CliResult {
         },
     };
 
-    let ws = try!(Workspace::new(&root, config));
-
     let err = try!(cargo_travis::run_coverage(&ws, &ops, &options.arg_args));
 
     match err {
@@ -149,10 +165,26 @@ fn execute(options: Options, config: &Config) -> CliResult {
 
 fn main() {
     env_logger::init().unwrap();
-    let config = Config::default().unwrap();
-    let args = env::args().collect::<Vec<_>>();
-    let result = call_main_without_stdin(execute, &config, USAGE, &args, false);
-    if let Err(e) = result {
-        exit_with_error(e, &mut *config.shell());
+    let config = match Config::default() {
+        Ok(cfg) => cfg,
+        Err(e) => {
+             let mut shell = cargo::core::Shell::new();
+             cargo::exit_with_error(e.into(), &mut shell)
+        }
+    };
+    let result = (|| {
+        let args: Vec<_> = try!(env::args_os()
+            .map(|s| {
+                s.into_string().map_err(|s| {
+                    CargoError::from(format!("invalid unicode in argument: {:?}", s))
+                })
+            })
+            .collect());
+        let rest = &args;
+        cargo::call_main_without_stdin(execute, &config, USAGE, rest, false)
+    })();
+    match result {
+        Err(e) => cargo::exit_with_error(e, &mut *config.shell()),
+        Ok(()) => {}
     }
 }
